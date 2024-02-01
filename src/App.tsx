190addc93a9464e7df7a43eeea249b5e307c989d
priyanka <tiwary.priyanka92Z@gmail.com>
2024-02-01 02:35:05 +0100

    added-responsive-to-autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
   const apiEndpoint = BASE_URL;
 
   return (
-    <div className="m-20 flex flex-col justify-center">
+    <div className="m-5 md:m-20 flex flex-col justify-center">
       <h1 className="text-2xl font-bold mb-4">AutoComplete Search</h1>
       <div className="bg-gray-100 p-8 mb-40 rounded-md shadow-md">
         <Autocomplete api_URL={apiEndpoint} searchCategory="Dog Breed" />
